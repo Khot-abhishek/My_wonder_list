dd9bba3300a44c63fdaf6328decabc86494a1c9e
khot-abhishek <abhishekrkhot@gmail.com>
2022-10-01 00:05:58 +0530

    Added the function to edit and delete the task from the task list

--- todo_list/base/views.py
+++ todo_list/base/views.py
@@ -1,8 +1,9 @@
+from audioop import reverse
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Task
 # Create your views here.
 
@@ -17,4 +18,13 @@ class TaskDetailView(DetailView):
 class TaskCreateView(CreateView):
     model = Task
     fields = "__all__"
+    success_url = reverse_lazy('task-list')
+
+class TaskUpdateView(UpdateView):
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy('task-list')
+    
+class TaskDeleteView(DeleteView):
+    model = Task
     success_url = reverse_lazy('task-list')
