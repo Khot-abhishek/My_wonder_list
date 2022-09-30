9654ae9ef540bf657724ef19a2be030cec0450eb
khot-abhishek <abhishekrkhot@gmail.com>
2022-09-30 23:42:48 +0530

    Added the task-list & detail functions (basic )

--- todo_list/base/urls.py
+++ todo_list/base/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.TaskListView.as_view(), name='task-list' ),
+    path('task-detail/<int:pk>/', views.TaskDetailView.as_view(), name="task-detail"),
 ]
--- todo_list/base/views.py
+++ todo_list/base/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from .models import Task
 # Create your views here.
 
 
 class TaskListView(ListView):
     model = Task
-    
+
+class TaskDetailView(DetailView):
+    model = Task    
+    context_object_name = 'task'
