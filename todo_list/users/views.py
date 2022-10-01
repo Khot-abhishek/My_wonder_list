0f1705055fd06acd0d4c4c439c6ab401207b70f0
khot-abhishek <abhishekrkhot@gmail.com>
2022-10-01 18:46:30 +0530

    added a registration feature with automatic login after the registration process

--- todo_list/users/views.py
+++ todo_list/users/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.contrib.auth.views import LoginView, LogoutView
+from django.views.generic.edit import FormView
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 from django.urls import reverse_lazy
+
 
 # Create your views here.
 
@@ -13,3 +17,16 @@ class UserLoginView(LoginView):
 
 class UserLogoutView(LogoutView):
     template_name = 'users/logout.html'    
+    
+    
+class UserRegistrationView(FormView):
+    form_class = UserCreationForm
+    template_name = 'users/register.html'    
+    success_url = reverse_lazy('task-list')
+    
+    def form_valid(self, form):
+        user = form.save()
+        if user is not None:
+            login(self.request, user)
+        print(form.cleaned_data)
+        return super().form_valid(form)
